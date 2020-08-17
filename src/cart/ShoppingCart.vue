4e273ffe0dbef4d67b0464685ab0d4c869afd713
emryssm <emrys.sm@gmail.com>
2020-08-16 23:26:38 -0400

    starting webpack section

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -18,7 +18,7 @@
             {{ robot.head.title }}
           </td>
           <td class="cost">
-            {{ robot.cost }}
+            {{ robot.cost | currency('$') }}
           </td>
         </tr>
       </tbody>
@@ -41,7 +41,7 @@
             {{ robot.head.title }}
           </td>
           <td class="cost">
-            {{ robot.cost }}
+            {{ robot.cost | currency('$') }}
           </td>
         </tr>
       </tbody>
@@ -50,6 +50,7 @@
 </template>
 
 <script>
+
 export default {
   name: "Cart",
   computed: {
@@ -58,7 +59,7 @@ export default {
     },
     cartSaleItems() {
       return this.$store.getters["robots/cartSaleItems"];
-    }
+    },
   },
 };
 </script>
--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,10 +1,6 @@
-export default {
-  bind: (element, binding) => {
-    if (binding.arg !== "position") return;
-
-    Object.keys(binding.value).forEach((position) => {
-      element.style[position] = binding.calue[position];
-    });
-    element.style.position = "absolute";
-  },
-};
+export default function(element, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    element.style[position] = binding.value[position];
+  });
+  element.style.position = "absolute";
+}
